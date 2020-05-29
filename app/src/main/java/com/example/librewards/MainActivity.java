91dd598c1cf5994f95e5b4656d457fde851be394
Arman Jalilian <armanjn123@gmail.com>
2020-05-29 20:20:44 +0100

    second

--- app/src/main/java/com/example/librewards/MainActivity.java
+++ app/src/main/java/com/example/librewards/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.librewards;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.sql.Time;
 
 public class MainActivity extends AppCompatActivity {
-
+BottomNavigationView bottomNavigation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+                    switch (menuItem.getItemId()){
+                        case R.id.navigation_timer:
+                            selectedFragment = new TimerFragment();
+                            break;
+                        case R.id.navigation_rewards:
+                            selectedFragment = new RewardsFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+                    return true;
+                }
+            };
+
+
 }
